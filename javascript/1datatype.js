7b7d299c34d112e395ee03fa713ea5ea2db4e789
aakashdeveloper <ahanda205@gmail.com>
2022-12-04 13:44:17 +0530

    javascript

---
+++ javascript/1datatype.js
@@ -0,0 +1,150 @@
+es5
+es6
+
+Ecma Script
+///////
+String = "Hii" 'Hey' "10" '10a' 'true' "false" `Test` 
+Number = 132 32423423  3.33 .343 
+Boolean = true false
+
+
+//es5
+
+///variable
+var a = 10
+var b = "Hii"
+var c = true
+
+var a = 10
+var b = "Hii"
+var c = true
+
+typeof(a)
+'number'
+typeof(b)
+'string'
+typeof(c)
+'boolean'
+var d = "76"
+typeof(d)
+'string'
+var d = 10.78687
+typeof(d)
+'number'
+
+var a = 10
+var b = 20
+
+a+b (addition)
+30
+a-b
+-10
+b-a
+10
+a*b
+200
+b/a
+2
+b%a
+0
+5%2
+1
+5%3
+2
+
+2%4
+2
+3%4
+3
+
+
+var a = "hii"
+var b = "JavaScript"
+a+b (concat)
+'hiiJavaScript'
+
+var c = 1
+undefined
+a+c
+'hii1'
+
+var a = "hii"
+var b = "JavaScript"
+undefined
+a+b
+'hiiJavaScript'
+a-b
+NaN
+a*b
+NaN
+a/b
+NaN
+
+string+string = string
+string+number = string
+number+string = string
+number+number = number
+
+
+10+"20"+30
+"1020"+30
+'102030'
+
+"10"+20+30
+"1020"+30
+'102030'
+
+10+20+"30"
+30+"30"
+'3030'
+
+"hiii"-1
+NaN (not a number)
+
+10+"20"+30-1
+"1020"+30
+"102030"-1
+102029
+
+"10"+20+30-1
+102029
+
+
+10+20+"30"-1
+30+"30"
+"3030"
+3029  
+
+"10"-1
+9
+10*"2"
+20
+"10"/"2"
+5
+"10"+"2"
+'102'
+
+"10a"-1
+NaN
+10*"2a"
+NaN
+"10a"/"2"
+
+
+var a = true
+var b = false
+var c = true
+
+true is equal to 1
+false is equal to 0
+
+a+c
+2
+a+b
+1
+
+
+10+true
+11
+10/false
+Infinity
