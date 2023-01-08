67fb9a1b2cb6cfd3d10881d0c26521cd803d60d7
aakashdeveloper <ahanda205@gmail.com>
2023-01-08 13:37:46 +0530

    mongo

--- node/restapi/app.js
+++ node/restapi/app.js
@@ -1,16 +1,42 @@
 let express = require('express');
 let app = express();
 let port = 9310;
+let cors = require('cors');
+let mongo = require('mongodb');
+let MongoClient = mongo.MongoClient;
+let mongoUrl = "mongodb://localhost:27017";
+let db;
+app.use(cors())
+
 
 app.get('/',(req,res) => {
     res.send('Hii from express')
 })
 
+// get all location
 app.get('/location',(req,res) => {
-    res.send('Hii from location')
+    db.collection('location').find().toArray((err,data) => {
+        if(err) throw err;
+        res.send(data)
+    })
 })
 
-app.listen(port,(err) => {
-    if(err) throw err;
-    console.log(`Server is running on port ${port}`)
+
+// get all Restaurants
+app.get('/restaurants',(req,res) => {
+    db.collection('restaurants').find().toArray((err,data) => {
+        if(err) throw err;
+        res.send(data)
+    })
 })
+
+//Connection with db
+MongoClient.connect(mongoUrl,(err,client) => {
+    if(err) console.log('Error While Connecting');
+    db = client.db('internfeb') // database name
+    app.listen(port,(err) => {
+        if(err) throw err;
+        console.log(`Server is running on port ${port}`)
+    })
+})
+
