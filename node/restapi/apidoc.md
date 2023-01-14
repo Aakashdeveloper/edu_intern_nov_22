ae94c2f9a3df0c47b55bf3d64b208b3ca60a04e8
aakashdeveloper <ahanda205@gmail.com>
2023-01-14 13:36:38 +0530

    api page1

--- node/restapi/apidoc.md
+++ node/restapi/apidoc.md
@@ -1,14 +1,19 @@
 # Page 1
 
 > List of city
+* http://localhost:9310/location
 > List of restaurant
+* http://localhost:9310/restaurants
 > Rest wrt to city
+* http://localhost:9310/restaurant/3
 > List of Meal Type
+* http://localhost:9310/mealtype
 
 
 # Page 2
 
 > Restaurants wrt to mealType
+* http://localhost:9310/restaurants?mealId=4
 > Restaurants wrt to mealType & Cuisine
 > Restaurants wrt to mealType & Cost
 > Sort on basis of price
@@ -16,7 +21,9 @@
 # Page3
 
 > Details of Restaurant
+* http://localhost:9310/details/12
 > Menu wrt to restaurant
+* http://localhost:9310/menu/12
 
 
 # Page 4
--- node/restapi/app.js
+++ node/restapi/app.js
@@ -21,10 +21,68 @@ app.get('/location',(req,res) => {
     })
 })
 
+// get all Restaurants
+app.get('/restaurant/:id',(req,res) => {
+    console.log(req.params.id)
+    let state_id = Number(req.params.id)
+    db.collection('restaurants').find({state_id:state_id}).toArray((err,data) => {
+        if(err) throw err;
+        res.send(data)
+    })
+})
+
 
 // get all Restaurants
 app.get('/restaurants',(req,res) => {
-    db.collection('restaurants').find().toArray((err,data) => {
+    console.log(req.query.stateId)
+    let query = {}
+    let stateId = Number(req.query.stateId)
+    let mealId = Number(req.query.mealId)
+    
+    if(stateId){
+        query={state_id:stateId}
+    }else if(mealId){
+        query={"mealTypes.mealtype_id":mealId}
+    }else{
+        query = {}
+    }
+    db.collection('restaurants').find(query).toArray((err,data) => {
+        if(err) throw err;
+        res.send(data)
+    })
+})
+
+// get all mealType
+app.get('/mealtype',(req,res) => {
+    db.collection('mealType').find().toArray((err,data) => {
+        if(err) throw err;
+        res.send(data)
+    })
+})
+
+// get all mealType
+app.get('/details/:restId',(req,res) => {
+    let restaurant_id = Number(req.params.restId);
+    /* http://localhost:9310/details/6288d22dbb17b75750d11caf
+    let id = mongo.ObjectId(req.params.restId);
+    */
+    db.collection('restaurants').find({restaurant_id}).toArray((err,data) => {
+        if(err) throw err;
+        res.send(data)
+    })
+})
+
+//// listing page data
+app.get('/filter/:mealId',(req,res)=>{
+    let mealId = Number(req.params.mealId);
+
+})
+
+
+// menu wrt to restaurants
+app.get('/menu/:restId',(req,res) => {
+    let rest_id = Number(req.params.restId)
+    db.collection('menu').find({restaurant_id:rest_id}).toArray((err,data) => {
         if(err) throw err;
         res.send(data)
     })
@@ -40,3 +98,14 @@ MongoClient.connect(mongoUrl,(err,client) => {
     })
 })
 
+
+/*
+app.get('/restaurant/:id',(req,res) => {
+    console.log(req.params.id)
+    let state_id = Number(req.params.id)
+    db.collection('restaurants').find({state_id:state_id}).toArray((err,data) => {
+        if(err) throw err;
+        res.send(data)
+    })
+})
+*/
