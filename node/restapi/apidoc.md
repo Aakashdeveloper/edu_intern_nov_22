4892b595b7cc6887188f4bd2024ed989df3ed88f
aakashdeveloper <ahanda205@gmail.com>
2023-01-15 14:08:23 +0530

    Api

--- node/restapi/apidoc.md
+++ node/restapi/apidoc.md
@@ -1,7 +1,7 @@
 # Page 1
 
 > List of city
-* http://localhost:9310/location
+* https://nodetestapi.onrender.com/location
 > List of restaurant
 * http://localhost:9310/restaurants
 > Rest wrt to city
@@ -15,8 +15,11 @@
 > Restaurants wrt to mealType
 * http://localhost:9310/restaurants?mealId=4
 > Restaurants wrt to mealType & Cuisine
+* http://localhost:9310/filter/1?cuisineId=5
 > Restaurants wrt to mealType & Cost
+* http://localhost:9310/filter/1?hcost=800&lcost=600
 > Sort on basis of price
+* http://localhost:9310/filter/1?hcost=600&lcost=100&sort=-1
 
 # Page3
 
@@ -28,10 +31,35 @@
 
 # Page 4
 > Menu Details
+* http://localhost:9310/menuItem
+{
+	"id":[5,9,12,14]
+}
 > Place Order
+* http://localhost:9310/placeOrder (POST)
+{
+	"orderId" : 5,
+	"name" : "Nikita",
+	"email" : "nikki@gmail.com",
+	"address" : "Hom 65",
+	"phone" : 8934645457,
+	"cost" : 391,
+	"menuItem" : [
+		89,34,23
+	]
+}
 
 # Page 5
 > List order
+* http://localhost:9310/orders
 > Order wrt to Email
+* http://localhost:9310/orders?email=ankit@gmail.com
 > Update payment details
+* http://localhost:9310/updateOrder/5
+{
+	"status":"Delivered",
+	"bank_name":"SBI",
+	"date":"16/01/2023"
+}
 > Delete orders
+* http://localhost:9310/deleteOrder/63c3aa5d33695db289869eea
--- node/restapi/app.js
+++ node/restapi/app.js
@@ -1,12 +1,18 @@
 let express = require('express');
 let app = express();
-let port = 9310;
 let cors = require('cors');
+let dotenv = require('dotenv');
+dotenv.config();
+let port = process.env.PORT || 9310;
 let mongo = require('mongodb');
 let MongoClient = mongo.MongoClient;
+let bodyParser = require('body-parser')
 let mongoUrl = "mongodb://localhost:27017";
 let db;
+
 app.use(cors())
+app.use(bodyParser.urlencoded({extended:true}))
+app.use(bodyParser.json())
 
 
 app.get('/',(req,res) => {
@@ -75,9 +81,38 @@ app.get('/details/:restId',(req,res) => {
 //// listing page data
 app.get('/filter/:mealId',(req,res)=>{
     let mealId = Number(req.params.mealId);
+    let cuisineId = Number(req.query.cuisineId);
+    let lcost = Number(req.query.lcost);
+    let hcost = Number(req.query.hcost);
+    let sort = {cost:1}
+    let query = {}
+
+    if(req.query.sort){
+        sort={cost:req.query.sort}
+    }
+    if(cuisineId){
+        query={
+            "mealTypes.mealtype_id":mealId,
+            "cuisines.cuisine_id":cuisineId
+        }
+    }else if(hcost && lcost){
+        query={
+            "mealTypes.mealtype_id":mealId,
+            "$and":[{cost:{$gt:lcost,$lt:hcost}}]
+        }
+    }
+    else{
+        query={
+            "mealTypes.mealtype_id":mealId
+        }
+    }
+
+    db.collection('restaurants').find(query).sort(sort).toArray((err,data) => {
+        if(err) throw err;
+        res.send(data)
+    })
 
 })
-
 
 // menu wrt to restaurants
 app.get('/menu/:restId',(req,res) => {
@@ -85,6 +120,66 @@ app.get('/menu/:restId',(req,res) => {
     db.collection('menu').find({restaurant_id:rest_id}).toArray((err,data) => {
         if(err) throw err;
         res.send(data)
+    })
+})
+
+//order
+app.get('/orders',(req,res) => {
+    let query = {}
+    let email = req.query.email;
+    if(email){
+        query={email}
+    }
+    db.collection('orders').find(query).toArray((err,data) => {
+        if(err) throw err;
+        res.send(data)
+    })
+})
+// menu details
+app.post('/menuItem',(req,res) =>{
+    if(Array.isArray(req.body.id)){
+        db.collection('menu').find({menu_id:{$in:req.body.id}}).toArray((err,result) => {
+            if(err) throw err;
+            res.send(result)
+        })
+    }else{
+        res.send('Invalid Input')
+    }
+})
+
+
+///place order
+app.post('/placeOrder',(req,res) => {
+    db.collection('orders').insert(req.body,(err,result) => {
+        if(err) throw err;
+        res.send('Order Placed')
+    })
+})
+
+// updateOrder
+app.put('/updateOrder/:id',(req,res) => {
+    let oid = Number(req.params.id);
+    db.collection('orders').updateOne(
+        {orderId:oid},
+        {
+            $set:{
+                "status":req.body.status,
+                "bank_name":req.body.bank_name,
+                "date":req.body.date
+            }
+        },(err,result) => {
+            if(err) throw err;
+            res.send('Order Updated')
+        }
+    )
+})
+
+//delete Order
+app.delete('/deleteOrder/:id',(req,res) => {
+    let _id = mongo.ObjectId(req.params.id);
+    db.collection('orders').remove({_id},(err,result) => {
+        if(err) throw err;
+        res.send('Order Deleted')
     })
 })
 
